938776d75f168d9885f1adcca917e46e31abcb78
vinaykornapalli <vinaykornapalli@gmail.com>
2019-07-01 20:10:06 +0530

    pluto: bare ticket structure

--- programs/pluto/ikev2_session_resume.c
+++ programs/pluto/ikev2_session_resume.c
@@ -1,3 +1,8 @@
 /*
  *  Helper functions and ticket store for IKEv2 Session Resumption
 */
+
+#include "ikev2_session_resume.h"
+#include "state.h"
+#include "packet.h"
+
--- programs/pluto/ikev2_session_resume.h
+++ programs/pluto/ikev2_session_resume.h
@@ -5,9 +5,32 @@
 #ifndef IKEV2_SESSION_RESUME_H
 #define IKEV2_SESSION_RESUME_H
 
+#include "state.h"
+#include "packet.h"
+
+/*
+ * There are two types for ticket:
+ * 1.Ticket by Value
+ * 2.Ticket by Reference
+ * Whenever a initiator(client) requests for a ticket the responder should select any of
+ * the above types and send it to client but client should not have any idea about the type 
+ * received.
 
 
+*/
 
+
+struct ticket_by_value {
+
+};
+
+struct ticket_by_reference {
+   uint8_t 
+};
+
+struct ticket_payload {
+    chunk_t ticket;
+};
 
 
 
